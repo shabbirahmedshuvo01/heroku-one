2ee3b6808726d5bf07a833f0a641484cd5887936
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-05-05 05:50:59 +0600

    adding steps

---
+++ __server_deploy.js
@@ -0,0 +1,14 @@
+/**
+ * ----------------------
+ * ONE TIME for your computer
+ * ----------------------
+ * 1. create heroku account 
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * --------------------------
+ * for each project one time 
+ * ---------------------------
+ * 1. heroku create 
+ * 2. 
+*/
